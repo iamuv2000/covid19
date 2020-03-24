f025b4c5802145dcf6f148fb2dbb1649c540bd90
iamuv2000 <yuvraj.singh2018@vitstudent.ac.in>
2020-03-24 11:33:46 +0530

    Integrated news section

--- lib/Screens/news.dart
+++ lib/Screens/news.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:covid19/services/NetworkHelper.dart';
+
 
 class NewsScreen extends StatefulWidget {
   @override
@@ -7,10 +9,80 @@ class NewsScreen extends StatefulWidget {
 
 class _NewsScreenState extends State<NewsScreen> {
 
+  Future <List> getData() async {
+    NetworkHelper covidData = NetworkHelper('http://newsapi.org/v2/top-headlines?country=in&q=corona&from=2020-02-24&sortBy=publishedAt&apiKey=d9fa391aacfe428e81b8c6002ea8a507');
+    var covidNews = await covidData.getData();
+    var articles = covidNews['articles'];
+    print(articles);
+    return articles;
+  }
+
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("News feature coming soon!"),
+      child: FutureBuilder(
+        future: getData(),
+        builder: (BuildContext context, AsyncSnapshot snapshot){
+          if(snapshot.data == null){
+            return
+                Container(
+                  child: Center(
+                    child: Text('Loading...'),
+                  )
+                );
+          }
+          else{
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index){
+                  return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Card(
+                      color: Colors.blueAccent.withOpacity(0.5),
+                      child: ListTile(
+                        title: Text(
+                            snapshot.data[index]['title'],
+                            style: TextStyle(
+                              fontSize: 20,
+                              letterSpacing: 1.2,
+                              color: Colors.white
+                            ),
+                        ),
+                          subtitle: Text(
+                              snapshot.data[index]['description'],
+                            style: TextStyle(
+                                fontSize: 14,
+                                letterSpacing: 0.9,
+                            ),
+                          ),
+                        trailing: Container(
+                          height: 50,
+                          child: Image.network(
+                            snapshot.data[index]['urlToImage'],
+                            scale: 1,
+                              filterQuality: FilterQuality.low,
+                            height: 50,
+                            fit: BoxFit.cover
+                          ),
+                        ),
+                        isThreeLine: true,
+                        contentPadding: EdgeInsets.all(10.0),
+                      ),
+                    ),
+                  );
+                }
+            );
+          }
+          }
+
+      )
     );
   }
 }
+//TODO: Integrate news api
